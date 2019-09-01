4b99f725f298c6c88d6f33b4758de67f13bf889c
Archana Nagaraja <archananagaraja@Archanas-MacBook-Air.local>
2019-09-01 15:26:46 -0700

    updated the port details

--- src/hello.py
+++ src/hello.py
@@ -4,4 +4,4 @@ app = Flask(__name__)
 def hello():
     return "Welcome!! Hello World!"
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int("5000"), debug=True)
+    app.run(host="0.0.0.0", port=int("5120"), debug=True)
